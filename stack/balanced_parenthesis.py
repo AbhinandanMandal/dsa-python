d13aa88051a8900e9c5f712f5980db6340d591ce
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-08-16 11:37:00 +0530

    balanced_parenthesis

--- stack/balanced_parenthesis.py
+++ stack/balanced_parenthesis.py
@@ -1,5 +1,5 @@
 
-
+"""
 def balancedParenthesis(expression):
     open_list = ["{", "[", "("]
     close_list = ["}", "]", ")"]
@@ -23,3 +23,26 @@ print(balancedParenthesis("{[]{()}}"))
 
 # Time complexity: O(N)
 # Space complexity: O(1)
+
+"""
+
+
+def BalancedParenthesis(expression):
+    opening = ["{", "[", "("]
+    closing = ["}", "]", ")"]
+    stack = []
+
+    for char in expression:
+        if char in opening:
+            stack.append(char)
+        elif char in closing:
+            pos = closing.index(char)
+
+            if stack and stack[-1] == opening[pos]:
+                stack.pop()
+            else:
+                return "Unbalanced"
+    return "Balanced" if not stack else "Unbalanced"
+
+
+print(BalancedParenthesis("{[]{()}}"))
