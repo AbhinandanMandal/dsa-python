55a0d25c3438f3b55cc3c7a23409bb5eb566dadb
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-09-09 00:31:38 +0530

    rotate array

--- Array/RotateArray.py
+++ Array/RotateArray.py
@@ -6,7 +6,7 @@ right rotate: putting the last d elements to the front
 """
 
 
-def LeftRotate(array: list, d: int):
+"""def LeftRotate(array: list, d: int):
     d = d % len(array)
     return array[d:]+array[:d]
 
@@ -18,5 +18,25 @@ def RightRotate(array: list, d: int):
 
 arr = [1, 2, 3, 4, 5]
 print(LeftRotate(arr, 3))
-print(RightRotate(arr, 3))
+print(RightRotate(arr, 3))"""
 
+# Left Rotate: putting first d element to the back of the array
+# That means last n-d element came to first
+
+
+def LeftRotate(array, d):
+    d = d % len(array)
+    return array[d:]+array[:d]
+
+
+def RightRotate(array, d):
+    d = d % len(array)
+    return array[-d:]+array[:-d]
+
+
+array = [1, 2, 3, 4, 5]
+print(LeftRotate(array, 3))
+print(RightRotate(array, 3))
+
+# Time complexity: O(n)
+# Space Complexity: O(n)
