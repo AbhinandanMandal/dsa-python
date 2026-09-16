0e233fd8d81fd0511b9ce289921bc2dcb4290f06
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-09-16 23:43:08 +0530

    maximum subarray

--- Array/MaximumSubarray.py
+++ Array/MaximumSubarray.py
@@ -1,16 +1,26 @@
 
-def maximumsubarray(array: list):
-    current = array[0]
-    maximum = array[0]
+""" 
+We need to find maximum subarray that returns the maximum subarray sum
+For the following array [-2, 1, -3, 4, -1, 2, 1, -5, 4], 
+It's [4, -1, 2, 1] returns maximum sum of 6
 
-    for num in array[1:]:
-        current = max(num, current+num)
-        maximum = max(maximum, current)
-    return maximum
+"""
 
 
-arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(maximumsubarray(arr))
+def MaximumSubarray(array):
+    maximum_sum = float('-inf')
+    maximum_subarray = []
 
-# Time complexity: O(n)
-# Space complexity: O(1)
+    for i in range(len(array)):
+        for j in range(len(array)):
+            array_sum = sum(array[i:j+1])
+            if array_sum > maximum_sum:
+                maximum_sum = array_sum
+                maximum_subarray = array[i:j+1]
+            elif array_sum == maximum_sum:  # Checking if two are equal then
+                maximum_subarray.append(array[i:j+1])
+    return maximum_subarray
+
+
+array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(MaximumSubarray(array))
